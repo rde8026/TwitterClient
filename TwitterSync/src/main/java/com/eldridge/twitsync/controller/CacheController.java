d94b20143428e4750fa807a1e964ee356f003677
Ryan Eldridge <rde8026@gmail.com>
2013-08-14 09:17:48 -0400

    swapped ArrayList for ArrayDeque for caching. still needs work

--- TwitterSync/src/main/java/com/eldridge/twitsync/controller/CacheController.java
+++ TwitterSync/src/main/java/com/eldridge/twitsync/controller/CacheController.java
@@ -10,7 +10,9 @@ import com.eldridge.twitsync.BuildConfig;
 import com.eldridge.twitsync.db.Tweet;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -29,15 +31,18 @@ public class CacheController {
     private static CacheController instance;
     private Context context;
 
-    private static final int CACHE_SIZE = 251;
+    private static final int CACHE_SIZE = 41;
 
     private static final int THREAD_POOL_SIZE = 20;
     private ExecutorService executorService;
     private static ArrayList<Tweet> memoryCache;
 
+    private static ArrayDeque<Tweet> deque;
+
     private CacheController() {
         executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         memoryCache = new ArrayList<Tweet>();
+        deque = new ArrayDeque<Tweet>();
         ActiveAndroid.setLoggingEnabled(true);
     }
 
@@ -51,19 +56,30 @@ public class CacheController {
         return instance;
     }
 
-    public void addToCache(final ResponseList<Status> items, boolean top) {
+    public synchronized void addToCache(final ResponseList<Status> items, boolean top) {
         try {
             long startTime = System.currentTimeMillis();
-            for (Status s : items) {
-                Tweet tweet = new Tweet();
-                tweet.tweetId = s.getId();
-                tweet.timestamp = s.getCreatedAt().getTime();
-                tweet.json = DataObjectFactory.getRawJSON(s);
-                addTweetToMemoryCache(tweet, top);
+            if (top) {
+                for (Status s : items) {
+                    Tweet tweet = new Tweet();
+                    tweet.tweetId = s.getId();
+                    tweet.timestamp = s.getCreatedAt().getTime();
+                    tweet.json = DataObjectFactory.getRawJSON(s);
+                    addTweetToMemoryCache(tweet, top);
+                }
+            } else {
+                for (int i = items.size() - 1; i >= 0; i--) {
+                    Status s = items.get(i);
+                    Tweet tweet = new Tweet();
+                    tweet.tweetId = s.getId();
+                    tweet.timestamp = s.getCreatedAt().getTime();
+                    tweet.json = DataObjectFactory.getRawJSON(s);
+                    addTweetToMemoryCache(tweet, top);
+                }
             }
             if (BuildConfig.DEBUG) {
                 long delta = System.currentTimeMillis() - startTime;
-                //Log.d(TAG, "** Cached " + items.size() + " to DB in " + delta + "ms **");
+                Log.d(TAG, "** Added " + items.size() + " to Cache **");
                 Log.d(TAG, "** Insert into memory cache took " + delta + " ms **");
             }
         } catch (IOException ioe) {
@@ -76,17 +92,17 @@ public class CacheController {
         return existing == null;
     }
 
-    private void addTweetToMemoryCache(Tweet t, boolean top) throws IOException {
+    private synchronized void addTweetToMemoryCache(Tweet t, boolean top) throws IOException {
         if (!memoryCache.contains(t)) {
             if (top) {
-                memoryCache.add(0, t);
+                deque.addFirst(t);
             } else {
-                memoryCache.add(t);
+                deque.addLast(t);
             }
         }
     }
 
-    public void trimCache() {
+    public synchronized void trimCache() {
         executorService.execute(new Runnable() {
             @Override
             public void run() {
@@ -95,25 +111,31 @@ public class CacheController {
                     ActiveAndroid.beginTransaction();
                     if (BuildConfig.DEBUG) {
                         Log.d(TAG, "** Persistent Cache cleared **");
-                        Log.d(TAG, "** Loading " + memoryCache.size() + " to persistent cache **");
+                        Log.d(TAG, "** Loading " + deque.size() + " to cache **");
                     }
-                    Log.d(TAG, "** Memory Cache Size: " + memoryCache.size() + " **");
-                    for (int i = 0; i < memoryCache.size(); i++) {
-                        if (i <= CACHE_SIZE) {
-                            Tweet t = memoryCache.get(i);
-                            //TODO: Fix this issue - either don't drop the entire DB or figure out a way to deal w/ the Id being populated.
-                            if (t.getId() != null) {
-                                Tweet hackForUpdate = new Tweet();
-                                hackForUpdate.timestamp = t.timestamp;
-                                hackForUpdate.tweetId = t.tweetId;
-                                hackForUpdate.json = t.json;
-                                hackForUpdate.save();
-                            } else {
-                                t.save();
-                            }
-                        } else {
+                    Iterator<Tweet> iterator = deque.iterator();
+                    int count = 0;
+                    while (iterator.hasNext()) {
+                        if (count >= CACHE_SIZE) {
+                            Log.d(TAG, "**** CACHE SIZE: " + CACHE_SIZE + " ****");
+                            Log.d(TAG, "****** Cache Size has been reached ******");
                             break;
                         }
+
+                        Tweet t = iterator.next();
+                        Status s = DataObjectFactory.createStatus(t.json);
+                        Log.d(TAG, "** Storing Tweet\n" + s.getText() + "\n **");
+                        if (t.getId() != null) {
+                            Log.d(TAG, "** Hack for ActiveAndroid **");
+                            Tweet hackForUpdate = new Tweet();
+                            hackForUpdate.timestamp = t.timestamp;
+                            hackForUpdate.tweetId = t.tweetId;
+                            hackForUpdate.json = t.json;
+                            hackForUpdate.save();
+                        } else {
+                            t.save();
+                        }
+                        count++;
                     }
                     ActiveAndroid.setTransactionSuccessful();
                 } catch (Exception e) {
@@ -124,20 +146,22 @@ public class CacheController {
                         List<Tweet> ts = new Select().from(Tweet.class).execute();
                         Log.d(TAG, "********** Cache Count is " + ts.size() + " **********");
                     }
-                    memoryCache.clear();
+                    //memoryCache.clear();
+                    deque.clear();
                 }
             }
         });
     }
 
-    public List<Status> getLatestCachedTweets() throws Exception {
+    public synchronized List<Status> getLatestCachedTweets() throws Exception {
         long startTime = System.currentTimeMillis();
-        List<Tweet> cachedTweets = new Select().from(Tweet.class).orderBy("Id ASC").execute();
+        List<Tweet> cachedTweets = new Select().from(Tweet.class).execute(); //.orderBy("Id ASC")
         List<Status> tweets = new ArrayList<Status>();
         for (Tweet t : cachedTweets) {
             Status s = DataObjectFactory.createStatus(t.json);
             tweets.add(s);
-            memoryCache.add(t);
+            //memoryCache.add(t);
+            deque.addFirst(t);
         }
         long delta = System.currentTimeMillis() - startTime;
         if (BuildConfig.DEBUG) {
@@ -147,19 +171,20 @@ public class CacheController {
         return tweets;
     }
 
-    private void deleteDbRecords() {
+    private synchronized void deleteDbRecords() {
         ActiveAndroid.beginTransaction();
         new Delete().from(Tweet.class).execute();
         ActiveAndroid.setTransactionSuccessful();
         ActiveAndroid.endTransaction();
     }
 
-    public void clearDb() {
+    public synchronized void clearDb() {
         executorService.execute(new Runnable() {
             @Override
             public void run() {
                 new Delete().from(Tweet.class).execute();
-                memoryCache.clear();
+                //memoryCache.clear();
+                deque.clear();
                 if (BuildConfig.DEBUG) {
                     Log.d(TAG, "*** Deleted ALL from cache db ***");
                 }
